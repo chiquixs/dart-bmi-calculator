a203163b6605ac688eeddd941fc3251b10909dd0
Nafisa Chiquita Finandra Putri <nafisachiquita06@gmail.com>
2026-03-09 23:24:21 +0700

    implement BMI calculation and category classification

--- lib/bmi_calculator.dart
+++ lib/bmi_calculator.dart
@@ -1,8 +1,24 @@
-Map<String, dynamic> hitungBMI(double tinggi, double berat) {
+Map<String, dynamic> hitungBMI(double tinggiCm, double beratKg) {
+  double tinggiM = tinggiCm / 100;
+
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  String kategori;
+
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+
   return {
-    'tinggi': tinggi,
-    'berat': berat,
-    'bmi': 0,
-    'kategori': ''
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi,
+    'kategori': kategori
   };
 }

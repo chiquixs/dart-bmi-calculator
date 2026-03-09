c449a489a99caaa98b0e91a95bfb699e382c4342
Nafisa Chiquita Finandra Putri <nafisachiquita06@gmail.com>
2026-03-09 23:20:45 +0700

    add input validation for height and weight

--- lib/input_handler.dart
+++ lib/input_handler.dart
@@ -1,7 +1,27 @@
+import 'dart:io';
+
 double inputTinggi() {
-  return 0;
+  while (true) {
+    stdout.write("Masukkan tinggi badan kamu (cm) : ");
+    double? tinggi = double.tryParse(stdin.readLineSync() ?? "");
+
+    if (tinggi != null && tinggi > 0) {
+      return tinggi;
+    }
+
+    print("Input tidak valid.\n");
+  }
 }
 
 double inputBerat() {
-  return 0;
+  while (true) {
+    stdout.write("Masukkan berat badan kamu (kg) : ");
+    double? berat = double.tryParse(stdin.readLineSync() ?? "");
+
+    if (berat != null && berat > 0) {
+      return berat;
+    }
+
+    print("Input tidak valid !\n");
+  }
 }
